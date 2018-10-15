199b46e93894d16aa74ec71fb4988974c0d67fef
Shawn Oster <shawn.oster@gmail.com>
2018-10-14 23:43:41 -0700

    added more details to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
     name = 'nightowlcli',
     version = '0.1.0',
+    description = ('Command-line interface project skeleton using best practices'),
+    author = 'Shawn Oster',
+    author_email = 'shawn.oster@gmail.com',
+    url = 'https://github.com/shawnoster/nightowlcli',
+    license = 'MIT License',
     packages = ['nightowlcli'],
     entry_points = {
         'console_scripts': [
