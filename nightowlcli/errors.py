7495fcd4f4ce48b8146a63213b0806a7226e8ff3
Shawn Oster <shawn.oster@gmail.com>
2018-12-04 14:12:53 -0500

    added default exception baseclass stub

---
+++ nightowlcli/errors.py
@@ -0,0 +1,9 @@
+"""
+Errors for the library.
+
+All exceptions defined by the library should be defined in this file.
+"""
+
+class Error(Exception):
+    """Base error for this module."""
+    pass
